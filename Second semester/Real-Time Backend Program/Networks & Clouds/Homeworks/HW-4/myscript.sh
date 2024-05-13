efa6a97f4380784db5bad17bc40a7a10aff160be
lamasalah32 <lamasalah32@gmail.com>
2024-05-13 21:09:24 +0300

    add new cloud homework (HW-4).

---
+++ Second semester/Real-Time Backend Program/Networks & Clouds/Homeworks/HW-4/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Select an operation:"
+echo "1) Launch htop on instance"
+echo "2) Launch tcpdump utility on instance"
+echo "3) Show EC2 instance metadata"
+read -p "Enter choice: " choice
+
+
+if [ "$choice" == "1" ]; then
+    echo "Launching htop..."
+    htop
+elif [ "$choice" == "2" ]; then
+    echo "Launching tcpdump..."
+    tcpdump
+elif [ "$choice" == "3" ]; then
+    echo "Retrieving instance metadata.."
+    # Metadata token retrieval
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+    # Fetching metadata
+    METADATA=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/)
+    echo "$METADATA"
+fi
